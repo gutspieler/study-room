5bd78216ba681482426f88b47d03aa1389182abf
gutspieler <balazspimpli@gmail.com>
2017-06-20 21:34:10 +0200

    add services, when add a new course api.service.createCourse not available

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,35 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Http, HttpModule } from '@angular/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
-import { CourseService } from './course.service';
-import { CoursesComponent } from './courses/courses.component';
-import { ApiService } from './api.service';
-import { Http } from '@angular/http';
+import { CourseListComponent } from './courses/course-list/course-list.component';
+import { ApiService } from './services/api.service';
+import { CourseDetailComponent } from './courses/course-detail/course-detail.component';
+import { NewCourseComponent } from './courses/new-course/new-course.component';
+import { CourseDataService } from './services/course-data.service';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    CoursesComponent
+    CourseListComponent,
+    CourseDetailComponent,
+    NewCourseComponent
   ],
   imports: [
+    HttpModule,
+    FormsModule,
     BrowserModule,
     AppRoutingModule
   ],
   providers: [
-    CourseService,
     ApiService,
-    Http
+    BrowserModule,
+    FormsModule,
+    CourseDataService
   ],
   bootstrap: [AppComponent]
 })
